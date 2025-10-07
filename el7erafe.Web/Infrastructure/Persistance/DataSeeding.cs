10538fcd9183603985a5fb9d353b9e0ef94a8add
Omar Osama <omarosama534@gmail.com>
2025-10-07 23:29:47 +0300

    Seeding Roles in Database

---
+++ el7erafe.Web/Infrastructure/Persistance/DataSeeding.cs
@@ -0,0 +1,35 @@
+﻿using DomainLayer.Contracts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Persistance.Databases;
+namespace Persistance
+{
+    public class DataSeeding(ApplicationDbContext _dbContext,
+        RoleManager<IdentityRole> _roleManager) : IDataSeeding
+    {
+        public async Task IdentityDataSeedingAsync()
+        {
+            try
+            {
+                var pendingMigration = await _dbContext.Database.GetAppliedMigrationsAsync();
+                if (pendingMigration.Any())
+                {
+                    await _dbContext.Database.MigrateAsync();
+                }
+                if (!_roleManager.Roles.Any())
+                {
+                    await _roleManager.CreateAsync(new IdentityRole("Client"));
+                    await _roleManager.CreateAsync(new IdentityRole("Admin"));
+                    await _roleManager.CreateAsync(new IdentityRole("Technician"));
+                }
+                await _dbContext.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ el7erafe.Web/el7erafe.Web/Extensions/DatabaseSeed.cs
@@ -0,0 +1,16 @@
+﻿using DomainLayer.Contracts;
+
+namespace el7erafe.Web.Extensions
+{
+    public static class DatabaseSeed
+    {
+        public static async Task<WebApplication> SeedDatabaseAsync(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var dataSeedingObject = scope.ServiceProvider.GetRequiredService<IDataSeeding>();
+            await dataSeedingObject.IdentityDataSeedingAsync();
+            return app;
+        }
+
+    }
+}
