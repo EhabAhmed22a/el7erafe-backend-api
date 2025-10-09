38e9a91e2e6332f251dfb3657ead99ab42ab5b95
Omar Osama <omarosama534@gmail.com>
2025-10-09 11:57:59 +0300

    Create Custom Exception Middleware Handler

--- el7erafe.Web/el7erafe.Web/Program.cs
+++ el7erafe.Web/el7erafe.Web/Program.cs
@@ -1,3 +1,4 @@
+using el7erafe.Web.CustomMiddleWares;
 using el7erafe.Web.Extensions;
 using el7erafe.Web.Mapper;
 using Persistance;
@@ -10,26 +11,28 @@ namespace el7erafe.Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            #region Add services to the container.
 
             builder.Services.AddControllers();
 
             #region Swagger Setup
             builder.Services.AddOpenApi();
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(options => 
+            builder.Services.AddSwaggerGen(options =>
                 options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "api.xml"))
             );
             #endregion
 
             //builder.Services.AddAutoMapper(typeof(MapperProfile));
-            builder.Services.AddPersistanceServices(builder.Configuration);
+            builder.Services.AddPersistanceServices(builder.Configuration); 
+            #endregion
 
             var app = builder.Build();
 
             await app.SeedDatabaseAsync();
 
-            // Configure the HTTP request pipeline.
+            #region Configure the HTTP request pipeline.
+            app.UseMiddleware<CustomExceptionHandlerMiddleWare>();
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
@@ -42,7 +45,8 @@ namespace el7erafe.Web
             //app.UseAuthorization();
 
 
-            app.MapControllers();
+            app.MapControllers(); 
+            #endregion
 
             app.Run();
         }
