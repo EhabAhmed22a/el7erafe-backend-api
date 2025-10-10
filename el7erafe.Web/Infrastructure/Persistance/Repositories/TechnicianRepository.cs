f3a157543a0231ce3e01730009c908f754910bdb
Omar Osama <omarosama534@gmail.com>
2025-10-10 19:10:53 +0300

    Update Technician Repository

--- el7erafe.Web/Infrastructure/Persistance/Repositories/TechnicianRepository.cs
+++ el7erafe.Web/Infrastructure/Persistance/Repositories/TechnicianRepository.cs
@@ -31,14 +31,14 @@ namespace Persistance.Repositories
                 .FirstOrDefaultAsync(t => t.UserId == userId);
         }
 
-        public async Task<IEnumerable<Technician>> GetAllAsync()
+        public async Task<IEnumerable<Technician>?> GetAllAsync()
         {
             return await _context.Set<Technician>()
                 .Include(t => t.User)
                 .ToListAsync();
         }
 
-        public async Task<IEnumerable<Technician>> GetAllByStatusAsync(TechnicianStatus status)
+        public async Task<IEnumerable<Technician>?> GetAllByStatusAsync(TechnicianStatus status)
         {
             return await _context.Set<Technician>()
                 .Include(t => t.User)
@@ -46,20 +46,21 @@ namespace Persistance.Repositories
                 .ToListAsync();
         }
 
-        public async Task UpdateAsync(Technician technician)
+        public async Task<int> UpdateAsync(Technician technician)
         {
             _context.Set<Technician>().Update(technician);
-            await _context.SaveChangesAsync();
+            return await _context.SaveChangesAsync();
         }
 
-        public async Task DeleteAsync(int id)
+        public async Task<int> DeleteAsync(int id)
         {
             var technician = await _context.Set<Technician>().FindAsync(id);
-            if (technician != null)
+            if (technician is not null)
             {
                 _context.Set<Technician>().Remove(technician);
-                await _context.SaveChangesAsync();
+                return await _context.SaveChangesAsync();
             }
+            return 0;
         }
 
         public async Task<bool> ExistsAsync(int id)
@@ -67,6 +68,11 @@ namespace Persistance.Repositories
             return await _context.Set<Technician>().AnyAsync(t => t.Id == id);
         }
 
+
+        public async Task<bool> ExistsAsync(string phoneNumber)
+        {
+            return await _context.Set<Technician>().AnyAsync(t => t.User.PhoneNumber == phoneNumber);
+        }
         public async Task<bool> ExistsByNationalIdAsync(string nationalId)
         {
             return await _context.Set<Technician>().AnyAsync(t => t.NationalId == nationalId);
