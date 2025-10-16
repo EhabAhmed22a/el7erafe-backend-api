d2a081dba6243dda7b100f8e319f6365ba0edb98
Omar Osama <omarosama534@gmail.com>
2025-10-16 14:38:41 +0300

    Create File Validation

--- el7erafe.Web/Shared/DataTransferObject/TechnicianIdentityDTOs/TechRegisterDTO.cs
+++ el7erafe.Web/Shared/DataTransferObject/TechnicianIdentityDTOs/TechRegisterDTO.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
 namespace Shared.DataTransferObject.TechnicianIdentityDTOs
 {
     public class TechRegisterDTO
