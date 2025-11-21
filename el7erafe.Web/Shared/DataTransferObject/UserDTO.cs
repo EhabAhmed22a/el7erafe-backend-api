c23a900dcf376f6be2bc98d3668a36b5a25f6704
Omar Osama <omarosama534@gmail.com>
2025-11-21 18:51:37 +0200

    Create UserDTO

---
+++ el7erafe.Web/Shared/DataTransferObject/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataTransferObject
+{
+    public class UserDTO
+    {
+        public string token { get; set; } = default!;
+        public int userId { get; set; } = default!;
+        public string userName { get; set; } = default!;
+        public string type { get; set; } = default!;
+    }
+}
