ab7bc44c1aeb12892f8e72181f32321795b90f17
Omar Osama <omarosama534@gmail.com>
2025-11-22 20:52:35 +0200

    Add ILogoutService Contract

---
+++ el7erafe.Web/Core/ServiceAbstraction/ILogoutService.cs
@@ -0,0 +1,9 @@
+﻿using Shared.DataTransferObject.LogoutDTOs;
+
+namespace ServiceAbstraction
+{
+    public interface ILOgoutService
+    {
+        Task<LogoutResponseDto> LogoutAsync(string token);
+    }
+}
