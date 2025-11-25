1644b84f9cd90a44e297beae76ae567d0a623641
Omar Osama <omarosama534@gmail.com>
2025-11-25 19:08:53 +0200

    Create IUserTokenRepository contract

---
+++ el7erafe.Web/Core/DomainLayer/Models/IdentityModule/IUserTokenRepository.cs
@@ -0,0 +1,16 @@
+﻿
+namespace DomainLayer.Models.IdentityModule
+{
+    public interface IUserTokenRepository
+    {
+        Task<UserToken> GetTokenAsync(string token);
+        Task<UserToken> GetUserTokenAsync(string userId);
+        Task<ApplicationUser> GetUserByTokenAsync(string token);
+        Task CreateTokenAsync(UserToken userToken);
+        Task UpdateTokenAsync(UserToken userToken);
+        Task DeleteTokenAsync(string token);
+        Task DeleteUserTokenAsync(string userId);
+        Task<bool> TokenExistsAsync(string token);
+        Task<bool> UserHasTokenAsync(string userId);
+    }
+}
