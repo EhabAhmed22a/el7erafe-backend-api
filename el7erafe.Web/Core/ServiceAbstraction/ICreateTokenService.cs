6f1b3a90cd25372c9df4c34c735faedf2a53abe7
Omar Osama <omarosama534@gmail.com>
2025-11-25 22:34:49 +0200

    Create ICreateTokenService Contract

---
+++ el7erafe.Web/Core/ServiceAbstraction/ICreateTokenService.cs
@@ -0,0 +1,11 @@
+﻿using DomainLayer.Models.IdentityModule;
+using DomainLayer.Models.IdentityModule.Enums;
+
+namespace ServiceAbstraction
+{
+    public interface ICreateTokenService
+    {
+        Task<string> CreateAndStoreTokenAsync(ApplicationUser user, TokenType tokenType);
+        Task<string> GenerateJwtTokenAsync(ApplicationUser user, TokenType tokenType);
+    }
+}
