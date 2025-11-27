26b99dac0659345fc517ca475392ea5dbc22ed34
Omar Osama <omarosama534@gmail.com>
2025-11-27 22:07:42 +0200

    Add rejections Model

---
+++ el7erafe.Web/Core/DomainLayer/Models/IdentityModule/Rejection.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace DomainLayer.Models.IdentityModule
+{
+    public class Rejection
+    {
+        public int Id { get; set; }
+
+        [StringLength(500)]
+        public string Reason { get; set; } = default!;
+        public int TechnicianId { get; set; }
+        public Technician Technician { get; set; } = default!;
+
+    }
+}
