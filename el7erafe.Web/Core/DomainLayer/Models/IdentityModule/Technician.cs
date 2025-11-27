6ebfbcd004e9abb2858386a1f8b210997c91c2cc
Omar Osama <omarosama534@gmail.com>
2025-11-27 21:56:17 +0200

    Update Technician Table

--- el7erafe.Web/Core/DomainLayer/Models/IdentityModule/Technician.cs
+++ el7erafe.Web/Core/DomainLayer/Models/IdentityModule/Technician.cs
@@ -7,8 +7,12 @@ namespace DomainLayer.Models.IdentityModule
         public int Id { get; set; }
         public string Name { get; set; } = default!;
         public string NationalIdFrontURL { get; set; } = default!;
+        public bool  IsNationalIdFrontVerified { get; set; } = false;
         public string NationalIdBackURL { get; set; } = default!;
+        public bool IsNationalIdBackVerified { get; set; } = false;
         public string CriminalHistoryURL { get; set; } = default!;
+        public bool IsCriminalHistoryVerified { get; set; } = false;
+        public int Rejection_Count { get; set; } = 0;
         public ApplicationUser User { get; set; } = default!;
         public string UserId { get; set; } = default!; //FK 
         public TechnicianStatus Status { get; set; }
---
+++ el7erafe.Web/Infrastructure/Persistance/Identity/Migrations/20251127195111_AddVerificationColumnsForFilesToTechnician.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistance.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVerificationColumnsForFilesToTechnician : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCriminalHistoryVerified",
+                table: "Technicians",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsNationalIdBackVerified",
+                table: "Technicians",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsNationalIdFrontVerified",
+                table: "Technicians",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rejection_Count",
+                table: "Technicians",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCriminalHistoryVerified",
+                table: "Technicians");
+
+            migrationBuilder.DropColumn(
+                name: "IsNationalIdBackVerified",
+                table: "Technicians");
+
+            migrationBuilder.DropColumn(
+                name: "IsNationalIdFrontVerified",
+                table: "Technicians");
+
+            migrationBuilder.DropColumn(
+                name: "Rejection_Count",
+                table: "Technicians");
+        }
+    }
+}
