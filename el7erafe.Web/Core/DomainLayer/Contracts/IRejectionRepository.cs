e23972263eb59fe982434bc9588c7b904699a3a0
Omar Osama <omarosama534@gmail.com>
2025-11-27 22:42:44 +0200

    Create rejection contract

---
+++ el7erafe.Web/Core/DomainLayer/Contracts/IRejectionRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using DomainLayer.Models.IdentityModule;
+
+namespace DomainLayer.Contracts
+{
+    public interface IRejectionRepository
+    {
+        Task<Rejection> GetByIdAsync(int id);
+        Task AddAsync(Rejection rejection);
+        Task UpdateAsync(Rejection rejection);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsForTechnicianAsync(int technicianId);
+    }
+}
