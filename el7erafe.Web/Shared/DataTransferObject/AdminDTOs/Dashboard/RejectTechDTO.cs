cd9f7013fd9e83ad206b24f1609efb603f1712d6
Omar Osama <omarosama534@gmail.com>
2025-12-03 19:58:59 +0200

    Create reject tech DTO

---
+++ el7erafe.Web/Shared/DataTransferObject/AdminDTOs/Dashboard/RejectTechDTO.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Shared.DataTransferObject.AdminDTOs.Dashboard
+{
+    public class RejectTechDTO
+    {
+        public string id { get; set; } = default!;
+        public bool? is_front_rejected { get; set; }
+        public bool? is_back_rejected { get; set; }
+        public bool? is_criminal_rejected { get; set; }
+    }
+}
