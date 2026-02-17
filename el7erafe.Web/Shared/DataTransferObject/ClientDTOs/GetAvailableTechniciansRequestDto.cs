5ba893787529a9a1f1789bdd475fafda21ef54b3
Omar Osama <omarosama534@gmail.com>
2026-02-17 23:55:46 +0200

    Add Validation for the GetAvailableTechnicianRequestDto

--- el7erafe.Web/Shared/DataTransferObject/ClientDTOs/GetAvailableTechniciansRequestDto.cs
+++ el7erafe.Web/Shared/DataTransferObject/ClientDTOs/GetAvailableTechniciansRequestDto.cs
@@ -1,10 +1,14 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Shared.DataTransferObject.ClientDTOs
 {
     public class GetAvailableTechniciansRequest
     {
+        [Required(ErrorMessage = "الخدمة مطلوبة")]
         public string ServiceName { get; set; } = default!;
 
+        [Required(ErrorMessage = "اسم المدينة مطلوب")]
         public string CityName { get; set; } = default!;
 
         public bool Sorted { get; set; } 
