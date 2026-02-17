fceee4621d2e1c7e094e4dcb57367c4759ba4863
Omar Osama <omarosama534@gmail.com>
2026-02-17 23:45:55 +0200

    Add DTOs for the getAvailableTechnicianRequest and the response that is AvailableTechnicians

---
+++ el7erafe.Web/Shared/DataTransferObject/ClientDTOs/GetAvailableTechniciansRequestDto.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Shared.DataTransferObject.ClientDTOs
+{
+    public class GetAvailableTechniciansRequest
+    {
+        public string ServiceName { get; set; } = default!;
+
+        public string CityName { get; set; } = default!;
+
+        public bool Sorted { get; set; } 
+    }
+
+}
