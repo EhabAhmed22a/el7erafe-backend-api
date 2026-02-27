48140cbcfdbf579a7ac5d536b8fd0c28518e909a
Omar Osama <omarosama534@gmail.com>
2026-02-27 23:35:01 +0200

    Add Chats Dtos for the chat service

---
+++ el7erafe.Web/Shared/DataTransferObject/ChatDTOs/MessageDto.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Shared.DataTransferObject.ChatDTOs
+{
+    public class MessageDto
+    {
+        public int Id { get; set; }
+        public int ChatId { get; set; }
+        public string SenderId { get; set; } = default!;
+        public string ReceiverId { get; set; } = default!;
+        public string Content { get; set; } = default!;
+        public string Type { get; set; } = default!; 
+        public DateTime CreatedAt { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
---
+++ el7erafe.Web/Shared/DataTransferObject/ChatDTOs/SendMessageDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DataTransferObject.ChatDTOs
+{
+    public class SendMessageDto
+    {
+        [Required]                      // Validation: Must have this!
+        public int ChatId { get; set; }
+
+        [Required]
+        public string SenderId { get; set; } = default!;
+
+        [Required]
+        public string ReceiverId { get; set; } = default!;
+
+        [Required]
+        [MaxLength(4000)]               // Message can't be too long
+        public string Content { get; set; } = default!;
+
+        public string Type { get; set; } = "Text"; // Default to text
+    }
+}
