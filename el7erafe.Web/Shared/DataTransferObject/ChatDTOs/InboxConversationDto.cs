9e17a3496529c60b1145e1af17fc0bea8e04e1ef
Omar Osama <omarosama534@gmail.com>
2026-03-01 01:24:36 +0200

    create InboxConversationDto

---
+++ el7erafe.Web/Shared/DataTransferObject/ChatDTOs/InboxConversationDto.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Shared.DataTransferObject.ChatDTOs
+{
+    public class InboxConversationDto
+    {
+        public int ChatId { get; set; }
+        public string ReceiverId { get; set; } = default!;
+        public string ReceiverName { get; set; } = default!;
+        public string? ReceiverImage { get; set; }
+        public string? LastMessageContent { get; set; }
+        public DateTime? LastMessageTime { get; set; }
+        public bool IsLastMessageFromMe { get; set; }
+        public int UnreadCount { get; set; }
+    }
+}
