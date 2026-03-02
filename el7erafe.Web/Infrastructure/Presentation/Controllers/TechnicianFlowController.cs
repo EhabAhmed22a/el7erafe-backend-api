e93b420f7df820eef24e1a62787603f0f9350fd4
Omar Osama <omarosama534@gmail.com>
2026-03-02 21:49:51 +0200

    Add chat inbox endpoint for technician

--- el7erafe.Web/Infrastructure/Presentation/Controllers/TechnicianFlowController.cs
+++ el7erafe.Web/Infrastructure/Presentation/Controllers/TechnicianFlowController.cs
@@ -1,16 +1,16 @@
-﻿
-
-using System.Security.Claims;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ServiceAbstraction;
+using ServiceAbstraction.Chat;
+using System.Security.Claims;
 
 namespace Presentation.Controllers
 {
     [ApiController]
     [Route("api/tech")]
     [Authorize(AuthenticationSchemes = "Bearer", Roles = "Technician")]
-    public class TechnicianFlowController(ITechnicianService technicianService):ControllerBase
+    public class TechnicianFlowController(ITechnicianService technicianService,
+                                          IChatService chatService): ControllerBase
     {
         [HttpGet("profile")]
         public async Task<IActionResult> GetProfile()
@@ -20,6 +20,19 @@ namespace Presentation.Controllers
                 return Unauthorized("المستخدم غير موجود");
 
             var result = await technicianService.GetProfile(userId);
+            return Ok(result);
+        }
+
+        [HttpGet("tf/inbox")]
+        public async Task<IActionResult> GetInbox()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(userId))
+                return Unauthorized();
+
+            var result = await chatService.GetInboxAsync(userId);
+
             return Ok(result);
         }
     }
