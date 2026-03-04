b18ce987482f5ce1101c0ad05eac695aec2a7ae7
Omar Osama <omarosama534@gmail.com>
2026-03-04 22:24:16 +0200

    Add message type for the sendMessage so now the frontend sends message type with the message

--- el7erafe.Web/Core/DomainLayer/Models/ChatModule/Enums/MessageType.cs
+++ el7erafe.Web/Core/DomainLayer/Models/ChatModule/Enums/MessageType.cs
@@ -4,9 +4,6 @@ namespace DomainLayer.Models.ChatModule.Enums
     public enum MessageType
     {
         Text = 1,
-        Image = 2,
-        Voice = 3,
-        Location = 4,
-        CallLink = 5
+        CallLink = 2
     }
 }
