c3f0627fee83d06a688a0cc8aee000795ecc68a6
Omar Osama <omarosama534@gmail.com>
2026-03-11 23:15:06 +0200

    Create TechnicianAvailability model and its Enum

---
+++ el7erafe.Web/Core/DomainLayer/Models/IdentityModule/TechnicianAvailability.cs
@@ -0,0 +1,15 @@
+﻿using DomainLayer.Models.IdentityModule.Enums;
+
+namespace DomainLayer.Models.IdentityModule
+{
+    public class TechnicianAvailability
+    {
+        public int Id { get; set; }
+        public string TechnicianId { get; set; } = default!;
+        public WeekDay? DayOfWeek { get; set; }
+        public TimeSpan FromTime { get; set; }
+        public TimeSpan ToTime { get; set; }
+
+        public Technician Technician { get; set; } = default!;
+    }
+}
