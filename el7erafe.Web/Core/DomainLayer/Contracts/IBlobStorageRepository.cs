eee770f73c81ad1e6503745ae24546af8f704966
AliMElSayed <167578531+AliMElSayed@users.noreply.github.com>
2026-04-17 20:13:27 +0200

    Revert "Revert "Add chat message moderation with regex and ML checks""

--- el7erafe.Web/Core/DomainLayer/Contracts/IBlobStorageRepository.cs
+++ el7erafe.Web/Core/DomainLayer/Contracts/IBlobStorageRepository.cs
@@ -19,6 +19,7 @@ namespace DomainLayer.Contracts
             int expiryHours = 1);
         Task<int> CountBlobsWithPrefixAsync(string containerName, string prefix);
         Task<int> DeleteBlobsWithPrefixAsync(string containerName, string prefix);
+        Task AppendToReviewListAsync(string text, float conf, string label);
 
     }
 }
